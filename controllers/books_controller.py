7075a8813733a7fa2abfa6aa313be1f7eeb82cad
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-25 18:41:31 +0000

    added functionality to delete book

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -10,7 +10,7 @@ books_blueprint = Blueprint("books", __name__)
 # INDEX
 # GET '/books'
 @books_blueprint.route("/books")
-def books():
+def get_books():
     books = book_repository.select_all()
     return render_template("books/index.html", books = books)
 
@@ -37,4 +37,9 @@ def books():
 
 # DELETE
 # DELETE '/books/<id>'
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+def delete_books(id):
+    book_repository.delete(id)
+    return redirect('/books')
+
 
