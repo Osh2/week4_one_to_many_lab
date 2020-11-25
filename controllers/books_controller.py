193d00947b4d82284c9000462584e8486680bdcd
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-25 20:02:53 +0000

    added route to add new books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
+from models.author import Author
 
 books_blueprint = Blueprint("books", __name__)
 
@@ -16,10 +17,32 @@ def get_books():
 
 # NEW
 # GET '/books/new'
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_task():
+    return render_template('books/new.html')
 
 
 # CREATE
 # POST '/books'
+@books_blueprint.route("/books", methods = ['POST'])
+def add_book_a():
+    first_name = request.form ['first_name']
+    last_name = request.form ['last_name']
+
+    author = Author(first_name, last_name)
+    author_repository.save(author)
+
+    title = request.form['title']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+
+    book = Book(title, genre, publisher, author)
+
+    book_repository.save(book)
+    return redirect('/books')
+
+    
+
 
 
 # SHOW
