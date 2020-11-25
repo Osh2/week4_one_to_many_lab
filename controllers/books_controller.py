8ff8b0c32ea59c3b4c6c379ee4c72db0cb0f28df
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-25 18:10:50 +0000

    created first routes and methods

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
+books_blueprint = Blueprint("books", __name__)
+
+
+
 # INDEX
 # GET '/books'
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", books = books)
 
 # NEW
 # GET '/books/new'
